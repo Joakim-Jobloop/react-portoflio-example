16f2f71eba3ab918e827c89588c6b8432958d39a
JoakimJobloop <Joakim@jobloop.no>
2023-10-26 13:48:30 +0200

    *

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar } from './components/navigation/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage'
 import { Footer } from './components/Footer'
+import { AboutPage } from './pages/AboutPage'
 
 function App() {
 const name = 'John Doe'
@@ -10,12 +11,17 @@ const name = 'John Doe'
   return (
     <>
     <Router>
+
     <Navbar/>
+
     <Routes>
       <Route path='/' element={<LandingPage/>} />
-      <Route path='/about' element={<h1>About</h1>} />
+      <Route path='/about' element={<AboutPage/>} />
+      <Route path='/else' element={<h1>Something else</h1>} />
     </Routes>
+
     <Footer/>
+
     </Router>
     </>
   )
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,8 @@
+export const AboutPage = () => {
+    return (
+        <main className="bg-gray-200 min-h-[calc(100vh-80px)] flex justify-center items-center">
+
+            <h1>This is the About page</h1>
+        </main>
+    )
+}
