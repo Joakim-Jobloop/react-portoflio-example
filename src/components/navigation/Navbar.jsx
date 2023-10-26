06268e2e13f2d1189a8f761b433e0843c334992e
JoakimJobloop <Joakim@jobloop.no>
2023-10-26 13:09:46 +0200

    *

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,6 +1,8 @@
 export const Navbar = () => {
-
-    return (
-        <nav className="bg-gray-400 h-20 flex justify-center items-center">This is the navbar</nav>
-    )
-}
+  return (
+    <nav className="bg-gray-400 h-20 flex justify-center items-center">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </nav>
+  );
+};
