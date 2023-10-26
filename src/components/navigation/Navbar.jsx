8bc49500d7fb100c104fc40b0b790c05c2f86179
JoakimJobloop <Joakim@jobloop.no>
2023-10-26 13:33:19 +0200

    *

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
   return (
     <nav className="bg-gray-400 h-20 flex justify-center items-center">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
     </nav>
   );
 };
