627e52e9682318d109b2a700e0839a0b01e8b472
JoakimJobloop <Joakim@jobloop.no>
2023-10-30 11:45:42 +0100

    *

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-center h-20 bg-gray-400">
+    <nav className="flex items-center justify-between h-20 px-16 bg-gray-400">
+      <img className="logo" src="/vite.svg" alt="logo" />
+      <div className="navitems">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
+      </div>
     </nav>
   );
 };
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,12 +5,15 @@ export const LandingPage = () => {
         <section className="p-12 flex-col gap-8 min-h-[calc(100vh-80px)] flex justify-center items-center">
           <h1 className="text-3xl min-w-[300px]">This is the landing page</h1>
           <img src="/vite.svg" alt="" />
+          <button>Click me</button>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum error facere modi tempore eligendi minus rem corrupti atque sapiente cupiditate, neque ratione quaerat ipsum, optio voluptas cumque quisquam officia sequi.</p>
         </section>
+       
         <section className="p-12 flex-col gap-8 min-h-[calc(100vh-80px)] flex justify-center items-center">
           <h1 className="text-3xl min-w-[300px]">Some other section</h1>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum error facere modi tempore eligendi minus rem corrupti atque sapiente cupiditate, neque ratione quaerat ipsum, optio voluptas cumque quisquam officia sequi.</p>
         </section>
+       
       </main>
 
       <style jsx>
